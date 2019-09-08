ca18e03f2805d7f08c0f3a95f36cb598955676e9
ikeikeikeike <jp.ne.co.jp@gmail.com>
2019-09-09 02:56:04 +0900

    returns error properly

--- util/rpc/rpc.go
+++ util/rpc/rpc.go
@@ -4,37 +4,13 @@ import (
 	"database/sql"
 	"time"
 
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/pkg/errors"
 
 	"github.com/ikeikeikeike/gocore/util/repo"
 )
-
-type (
-	// Mux uses grpc mux
-	Mux struct {
-		GrpcMux      *grpc.Server
-		GrpcEndpoint string
-		GwMux        *runtime.ServeMux
-		GwOpts       []grpc.DialOption
-		GwEndpoint   string
-	}
-
-	// Muxer has gRPC configuration
-	Muxer struct {
-		API     *Mux
-		Company *Mux
-	}
-)
-
-// AsList simply list converter
-func (m *Muxer) AsList() []*Mux {
-	return []*Mux{m.API, m.Company}
-}
 
 // GeneralTimeout context timeout seconds
 var GeneralTimeout = time.Second * 30
@@ -53,7 +29,7 @@ func GRPCError(err error) error {
 		return status.Error(codes.AlreadyExists, err.Error())
 	}
 	if err != nil {
-		return status.Error(codes.InvalidArgument, err.Error())
+		return status.Error(codes.Unknown, err.Error())
 	}
 
 	return nil
